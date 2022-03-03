d5159d439e007582837c435488950b58f704029d
oscamtor <oscamtor@gmail.com>
2022-03-03 18:12:57 +0100

    Application Output (Hello, World!) obtained with C++ std::cout  instead of C printf()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+// Estos 2 para std::cout ...
+#include <iostream>
+#include <sstream>
+
+// ... O este para printf()
 #include <stdio.h>
 
 int main() {
-    //std::cout << "Hello, World!" << std::endl;
-    printf("Hola mundo!");
+    std::cout << "Hello, World!" << std::endl;
+    //printf("Hola mundo!");
     return 0;
 }
